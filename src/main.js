83768f3aa79a16546bf017f7a4ec4b2805decd03
Gerben Op den Kamp <gerbenopdenkamp@Hotmail.com>
2021-12-03 13:33:13 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onRegisterError(error) {}
+})
+
 createApp(App).mount('#app')
